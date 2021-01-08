845fa9d222ebb9856ea9f7bc8e7dddc9790a0fa6
Maurits Van Der Steur <mauritsvdsteur@live.com>
2021-01-08 20:07:26 +0400

    some fixes -- works well

--- chess_image_ML.py
+++ chess_image_ML.py
@@ -17,9 +17,9 @@ from keras import backend as K
 from keras.layers.core import Activation
 from keras.optimizers import Adam
 
-total_samples = 2
+total_samples = 500
 
-#TODO fix this
+#TODO fix this paths
 def load_test():
     image_full = Image.open(r"A:\repo\chess-image-ML\test.png")
     image_grayscale = image_full.convert('L')
@@ -59,8 +59,8 @@ def load_image_labels():
             if i >= total_samples*2:  # 12 rows of labels per sample
                 break
 
-    arr = np.asarray(labels).astype('float32').reshape(-1, 64)#reshape(total_samples, 2, 64)
-    arr1 = arr[0::2].copy()
+    arr = np.asarray(labels).astype('float32').reshape(-1, 64)
+    arr1 = arr[0::2].copy() #TODO is this correct?
     arr2 = arr[1::2].copy()
     return arr1, arr2
 
@@ -98,21 +98,21 @@ def make_default_hidden_layers(inputs):
     x = LeakyReLU(alpha=0.1)(x)
     #x = BatchNormalization(axis=-1)(x)
     x = MaxPooling2D(pool_size=(3, 3))(x)
-    #x = Dropout(0.25)(x)
+    x = Dropout(0.4)(x)
 
     x = Conv2D(256, (3, 3), padding="same")(x)
     x = Activation("relu")(x)
     x = LeakyReLU(alpha=0.1)(x)
     #x = BatchNormalization(axis=-1)(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    #x = Dropout(0.25)(x)
+    x = Dropout(0.4)(x)
 
-    x = Conv2D(512, (3, 3), padding="same")(x)
+    x = Conv2D(256, (3, 3), padding="same")(x)
     x = Activation("relu")(x)
     x = LeakyReLU(alpha=0.1)(x)
     #x = BatchNormalization(axis=-1)(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    #x = Dropout(0.25)(x)
+    x = Dropout(0.4)(x)
 
     return x
 
@@ -125,14 +125,10 @@ def build_generic_branch(inputs, num_classes, name):
     """
     x = make_default_hidden_layers(inputs)
     x = Flatten()(x)
-    x = Dense(256)(x)
-    x = Activation("relu")(x)
-    x = LeakyReLU(alpha=0.1)(x)
-
-    x = Flatten()(x)
     x = Dense(128)(x)
     x = Activation("relu")(x)
     x = LeakyReLU(alpha=0.1)(x)
+    x = Dropout(0.4)(x)
 
     x = Dense(num_classes)(x)
     x = Activation("sigmoid", name=name)(x) #sigmoid is better for multi-label classification?
@@ -153,17 +149,17 @@ def assemble_full_model(input_shape, num_classes):
 
 def train_model(model, train_x, train_y):
     init_lr = 1e-4
-    epochs = 50
-    batch_size = 1
+    epochs = 20
+    batch_size = 16
 
-    opt = Adam()
-    #opt = Adam(lr=init_lr, decay=init_lr / epochs) # TODO
+    #opt = Adam()
+    opt = Adam(lr=init_lr, decay=init_lr / epochs) # TODO
     #loss has to be a regression loss?
     #TODO names
     model.compile(optimizer=opt, loss={
                   'white_pawn': 'binary_crossentropy',
                   'black_pawn': 'binary_crossentropy', },
-                  metrics=['accuracy'])
+                  metrics=[keras.metrics.BinaryAccuracy()])
     model.summary()
 
     #train_X, valid_X, train_label, valid_label = train_test_split(train_x, train_y, test_size=0.2, random_state=44)
@@ -171,70 +167,21 @@ def train_model(model, train_x, train_y):
     train_X = train_x
 
     t1 = train_y[0]
-    t2 = train_y[1] # TODO this is where it breaks with 2+ samples?
-    train = model.fit(train_X, [t1, t2], batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0)
+    t2 = train_y[1]
+    train = model.fit(train_X, [t1, t2], batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.2)
     model.save("chess_model2.h5py")
 
     return model, train
 
 
-def run_machine_learning(train_x, train_y, input_shape):
-    train_X, valid_X, train_label, valid_label = train_test_split(train_x, train_y, test_size=0.2,
-                                                                  random_state=44)
-
-    batch_size = 1
-    epochs = 20
-    num_classes = 64  # 64 squares total?
-
-    chess_model = Sequential()
-    chess_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=input_shape, padding='same'))
-    chess_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
-    chess_model.add(LeakyReLU(alpha=0.1))
-    chess_model.add(MaxPooling2D((2, 2), padding='same'))
-
-    chess_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
-    chess_model.add(LeakyReLU(alpha=0.1))
-    chess_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-
-    chess_model.add(Conv2D(256, (3, 3), activation='linear', padding='same'))
-    chess_model.add(LeakyReLU(alpha=0.1))
-    chess_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-
-    chess_model.add(Conv2D(512, (3, 3), activation='linear', padding='same'))
-    chess_model.add(LeakyReLU(alpha=0.1))
-    chess_model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-
-    chess_model.add(Flatten())
-    chess_model.add(Dense(256, activation='linear'))
-    chess_model.add(LeakyReLU(alpha=0.1))
-
-    chess_model.add(Flatten())
-    chess_model.add(Dense(128, activation=relu_advanced))
-    chess_model.add(LeakyReLU(alpha=0.1))
-    chess_model.add(Dense(num_classes, activation=relu_advanced))
-
-    # try poisson with other activations
-    ##MeanSquaredLogarithmicError
-    chess_model.compile(loss=keras.losses.MeanSquaredError(), optimizer=keras.optimizers.Adam(),
-                        metrics=['accuracy'])
-
-    chess_model.summary()
-    # train!
-    chess_train = chess_model.fit(x=train_X, y=train_label, batch_size=batch_size, epochs=epochs, verbose=1,
-                                  validation_data=(valid_X, valid_label))
-    chess_model.save("chess_model.h5py")
-
-    return chess_model, chess_train
-
-
 def display_results(model, train, test_x, test_y, names):
-    test_eval = model.evaluate(test_x, test_y, verbose=0)
-    print('Test loss:', test_eval[0])
-    print('Test accuracy:', test_eval[1])
+    #test_eval = model.evaluate(test_x, test_y, verbose=0)
+    #print('Test loss:', test_eval[0])
+    #print('Test accuracy:', test_eval[1])
 
     for idx, name in enumerate(names):
-        accuracy = train.history[name + '_accuracy']
-        val_accuracy = train.history['val_' + name + '_accuracy']
+        accuracy = train.history[name + '_binary_accuracy']
+        val_accuracy = train.history['val_' + name + '_binary_accuracy']
         epochs = range(len(accuracy))
 
         subplot_idx = 120 + idx + 1 # TODO scale this with more names
@@ -249,7 +196,7 @@ def display_results(model, train, test_x, test_y, names):
     for idx, name in enumerate(names):
         loss = train.history[name + '_loss']
         val_loss = train.history['val_' + name + '_loss']
-        accuracy = train.history[name + '_accuracy']
+        accuracy = train.history[name + '_binary_accuracy']
         epochs = range(len(accuracy))
 
         subplot_idx = 120 + idx + 1  # TODO scale this with more names
@@ -277,9 +224,9 @@ chess_model = assemble_full_model(in_shape, 64)  # 64 squares, num of classes
 ml_model, ml_train = train_model(chess_model, training_data, [train_Y1, train_Y2])
 
 #ml_model, ml_train = run_machine_learning(training_data, train_Y, in_shape)
-#display_results(ml_model, ml_train, test_X, test_Y, ["white_pawn", "black_pawn"])
+display_results(ml_model, ml_train, test_X, test_Y, ["white_pawn", "black_pawn"])
 
-predicted_classes = ml_model.predict(test_X)
+predicted_classes = ml_model.predict(training_data[0].reshape(1, 100, 100, 1)) # test_X
 np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 print(predicted_classes)
